eb3d0f00680f9957bbdd77e761fe7940d5713a7b
Axel Greavette <axelgreavette@gmail.com>
2023-03-05 08:58:15 -0500

    More

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,8 +2,11 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.event.BooleanEvent;
 import edu.wpi.first.wpilibj2.command.*;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
+import frc.robot.Constants.*;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 import frc.robot.util.HandleSignal;
@@ -12,36 +15,44 @@ import frc.robot.util.HandleSignal;
  * Handles command button-assignment.
  */
 public class RobotContainer {
-    private Joystick DriveController = new Joystick(0);
-    private XboxController CopilotController = new XboxController(1);
+    private Joystick DriveController = new Joystick(USB.DRIVER);
+    private XboxController CopilotController = new XboxController(USB.COPILOT);
 
     private final DriveBase driveBase = new DriveBase();
     private final Arm arm = new Arm();
     private final Clamp clamp = new Clamp();
 
     public RobotContainer() {
-        driveBase.setDefaultCommand(new DriveWithJoystick(driveBase, () -> (1 + (-DriveController.getRawAxis(3))) / 2, () -> DriveController.getRawAxis(0), () -> DriveController.getRawAxis(1)));
-        clamp.setDefaultCommand(new ClampMovement(clamp, () -> HandleSignal.ClampTrigger(CopilotController.leftTrigger(null).getAsBoolean(), CopilotController.rightTrigger(null).getAsBoolean())));
-        arm.setDefaultCommand(new ArmRotation(arm, () -> HandleSignal.ArmButton(DriveController.getRawButton(1), DriveController.getRawButton(4))));
+        driveBase.setDefaultCommand(new DriveWithJoystick(driveBase, () -> (1 + (-DriveController.getRawAxis(Controls.JOYSTICK_X_AXIS))) / 2, () -> DriveController.getRawAxis(Controls.JOYSTICK_Y_AXIS), () -> DriveController.getRawAxis(Controls.JOYSTICK_THROTTLE)));
+        //clamp.setDefaultCommand(new ClampMovement(clamp, () -> HandleSignal.ClampTrigger(CopilotController.leftTrigger(null).getAsBoolean(), CopilotController.rightTrigger(null).getAsBoolean())));
+        //arm.setDefaultCommand(new ArmRotation(arm, () -> HandleSignal.ArmButton(DriveController.getRawButton(1), DriveController.getRawButton(4))));
+    
+        JoystickButton ClampOpen = new JoystickButton(CopilotController, Controls.CLAMP_OPEN);
+        JoystickButton ClampClose = new JoystickButton(CopilotController, Controls.CLAMP_CLOSE);
+
+        ClampOpen.onTrue(new InstantCommand(() -> {
+            new ClampMovement(clamp, () -> 200);
+        }));
+        ClampClose.onTrue(new InstantCommand(() -> {
+            new ClampMovement(clamp, () -> 400);
+        }));
+
+        JoystickButton ArmUp = new JoystickButton(CopilotController, Controls.ARM_UP);
+        JoystickButton ArmDown = new JoystickButton(CopilotController, Controls.ARM_DOWN);
+
+        ArmUp.onTrue(new InstantCommand(() -> {
+            new ManualArmRotation(arm, () -> 200);
+        }));
+        ArmDown.onTrue(new InstantCommand(() -> {
+            new ManualArmRotation(arm, () -> 400);
+        }));
+
     }
 
     public Command getAutonomousCommand() {
         return new SequentialCommandGroup(
-            new InstantCommand(() -> new DriveForTime(driveBase, 0.4, 4))
+            new InstantCommand(() -> new DriveForTime(driveBase, 0.4, 4)),
+            new InstantCommand(() -> arm.setPos(30))
         );
-        
-        /* 
-                new InstantCommand(() -> shooter.openBallGate()),
-                new ParallelCommandGroup(
-                        new TurnTurretForTime(turret, 0.22, 4),
-                        new DriveForTime(driveBase, 0.6, 4)),
-                new AlignTurret(turret, limelight, true),
-                new ShooterRev(shooter, limelight),
-                new TimedIntake(intake, 1.0, 0.22, TimedIntake.IntakeMode.INTERNAL),
-                new ShooterRev(shooter, limelight),
-                new TimedIntake(intake, 1.0, 0.15, TimedIntake.IntakeMode.INTERNAL),
-                new ShooterRev(shooter, limelight),
-                new TimedIntake(intake, 1.0, 0.15, TimedIntake.IntakeMode.INTERNAL),
-                new InstantCommand(() -> intake.setRaised(false)));*/
     }
 } 
--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -2,14 +2,18 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import frc.robot.Constants.CanIDs;
+
 public class DriveBase extends SubsystemBase {
-    private final CANSparkMax left0 = new CANSparkMax(40, MotorType.kBrushless);
-    private final CANSparkMax right0 = new CANSparkMax(23, MotorType.kBrushless);
-    private final CANSparkMax left1 = new CANSparkMax(20, MotorType.kBrushless);
-    private final CANSparkMax right1 = new CANSparkMax(21, MotorType.kBrushless);
+    private final CANSparkMax left0 = new CANSparkMax(CanIDs.LEFT_DRIVE_0, MotorType.kBrushless);
+    private final CANSparkMax right0 = new CANSparkMax(CanIDs.RIGHT_DRIVE_0, MotorType.kBrushless);
+    private final CANSparkMax left1 = new CANSparkMax(CanIDs.LEFT_DRIVE_1, MotorType.kBrushless);
+    private final CANSparkMax right1 = new CANSparkMax(CanIDs.RIGHT_DRIVE_1, MotorType.kBrushless);
+
+    private final DifferentialDrive drive = new DifferentialDrive(left0, right0);
 
     public DriveBase() {
         left0.restoreFactoryDefaults();
@@ -17,8 +21,10 @@ public class DriveBase extends SubsystemBase {
         right0.restoreFactoryDefaults();
         right1.restoreFactoryDefaults();
 
+        right0.setInverted(true);
+
         left1.follow(left0);
-        right1.follow(left0);
+        right1.follow(right0);
     }
 
     /**
@@ -29,7 +35,7 @@ public class DriveBase extends SubsystemBase {
      */
     public void setSpeed(double x, double y, double throttle) {
         left0.set((x + y) * throttle);
-        right0.set((x - y) * throttle);
+        right0.set((x + y) * throttle);
     }
 
     /**
@@ -41,5 +47,16 @@ public class DriveBase extends SubsystemBase {
     public void setSpeed(double speed, double throttle) {
         left0.set(speed * throttle);
         right0.set(speed * throttle);
+    }
+    /**
+     * Another way to do this, using the built in WPILib solution.
+     * @param throttle throttle value
+     * @param rotation z-axis rotation value
+     * @param squareInputs boolean square inputs, defaults to false
+     */
+    public void setSpeed(double throttle, double rotation, boolean squareInputs) {
+        rotation *= -1;
+
+        drive.arcadeDrive(throttle, rotation, squareInputs);
     }
 }
